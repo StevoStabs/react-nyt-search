9e19144fb16fc10fc1bd03f70e81eb54fc866a1b
Steven Stabile <stevenstabile@gmail.com>
2016-06-14 13:12:58 -0400

    got search working

---
+++ app/components/Saved.js
@@ -0,0 +1,18 @@
+var React 		= require('react');
+var ReactDOM    = require('react-dom');
+
+
+var Saved = React.createClass({
+
+	render: function(){
+
+		return (
+
+			<p>SAVED TEST</p>
+			
+			)
+	}
+
+});
+
+module.exports = Saved; 
